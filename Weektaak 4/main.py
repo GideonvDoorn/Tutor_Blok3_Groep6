2937fccd38f36de77dc0a7964e50a4bdf109ab7d
GideonvDoorn <gideonvd7@gmail.com>
2022-02-23 11:20:39 +0100

    sequence paths

--- Weektaak 4/main.py
+++ Weektaak 4/main.py
@@ -49,17 +49,37 @@ def get_sequences_from_file(file_name):
 
 def print_protein_count(protein_sequence):
 
+    print("-"*80)
+    print(protein_sequence.get_header())
+
     for aa in aa_data:
         letter = aa.get_letter()
         print(f"{letter} : {protein_sequence.percentage_by_letter(letter)}")
 
+    print(f"Hydrophobic: {protein_sequence.percentage_by_class(AAClass.hydrophobic)}%")
+    print("")
+
 if __name__ == '__main__':
 
     fumh_path = "sequences/FUMH_HUMAN_protein.fasta"
+    gtr8_path = "sequences/GTR8_HUMAN_protein.fasta"
+    hrh3_path = "sequences/HRH3_HUMAN_protein.fasta"
+    ntrk1_path = "sequences/NTRK1_HUMAN_protein.fasta"
+
+    hiv1_path = "sequences/HIV-1 coding sequence protein.txt"
+    hiv2_path = "sequences/HIV-2 coding sequence protein.txt"
+    siv1_path = "sequences/SIV-1 coding sequence protein.txt"
+    siv2_path = "sequences/SIV-2 coding sequence protein.txt"
 
     fumh_sequence = get_sequences_from_file(fumh_path)[0]
+    gtr8_sequence = get_sequences_from_file(gtr8_path)[0]
+    hrh3_sequence = get_sequences_from_file(hrh3_path)[0]
+    ntrk1_sequence = get_sequences_from_file(ntrk1_path)[0]
 
     print_protein_count(fumh_sequence)
-    print(fumh_sequence.percentage_by_class(AAClass.inbetween))
+    print_protein_count(gtr8_sequence)
+    print_protein_count(hrh3_sequence)
+    print_protein_count(ntrk1_sequence)
+
 
 
